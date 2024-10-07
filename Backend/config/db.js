70b8910276a694b7c44b5dcc35c480095483f173
Navjot Gill <navjotkaurgill41@gmail.com>
2024-10-06 21:58:03 -0400

    Modified db.js file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://navjotkaurgill41:SkyYvhWdulNigQQZ@cluster0.xbyyt.mongodb.net/capstone').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 }
